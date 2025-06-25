d210831e64a9636bd83f8036f9f12a4f80c12072
Andresv27728 <andrescamilovallejogomez701@gmail.com>
2025-06-24 22:39:15 -0500

    Update config.js

--- config.js
+++ config.js
@@ -43,9 +43,9 @@ global.miniurl = fs.readFileSync('./storage/img/miniurl.jpg')
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.group = 'https://chat.whatsapp.com/C92isvspFcXCtqv2PqCfHI'
-global.canal = 'https://whatsapp.com/channel/0029VaGt7Uk6WaKkEDZUh43W'
-global.insta = 'https://instagram.com/adri.analegresanchez'
+global.group = 'https://chat.whatsapp.com/DPDcHF49QoJ7KzwHvFBjiW'
+global.canal = 'https://whatsapp.com/channel/0029Vb5atcVL7UVQwAB9tU2k'
+global.insta = 'https://Instagram.com'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
@@ -53,7 +53,7 @@ global.catalogo = fs.readFileSync('./storage/img/catalogo.png');
 global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 
 global.ch = {
-ch1:'120363209036072837@newsletter'
+ch1'120363419172960653@newsletter'
 }
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
